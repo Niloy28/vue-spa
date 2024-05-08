2112096aef67f3a75e8f71cb08d80068d7e08910
Farhan Ishrak Islam <niloy28@live.com>
2024-05-08 15:31:54 +0900

    Initial Project Setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>Hello World</p>
+</template>
+
+<script>
+export default {
+  /**
+   * A method that returns an object with a message property set to "Hello World".
+   *
+   * @return {object} An object with a message property.
+   */
+  data() {
+    return {
+      message: 'Hello World'
+    }
+  }
+}
+</script>
