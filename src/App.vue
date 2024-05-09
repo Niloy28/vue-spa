845330410603345e71ba911a5d4d6a9f165996e7
Farhan Ishrak Islam <niloy28@live.com>
2024-05-09 10:41:37 +0900

    NavBar と PageViewer を作成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <p>Hello World</p>
+  <NavBar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => (activePage = index)"
+  ></NavBar>
+
+  <PageViewer :page="pages[activePage]"></PageViewer>
 </template>
 
 <script>
+import PageViewer from './components/PageViewer.vue'
+import NavBar from './components/NavBar.vue'
+
 export default {
-  /**
-   * A method that returns an object with a message property set to "Hello World".
-   *
-   * @return {object} An object with a message property.
-   */
+  components: {
+    NavBar,
+    PageViewer
+  },
   data() {
     return {
-      message: 'Hello World'
+      activePage: 0,
+      pages: [
+        {
+          link: { text: 'Home', url: 'index.html' },
+          pageTitle: 'Home',
+          pageContent: 'This is the home page'
+        },
+        {
+          link: { text: 'About', url: 'about.html' },
+          pageTitle: 'About',
+          pageContent: 'This is the about page'
+        },
+        {
+          link: { text: 'Contacts', url: 'contact.html' },
+          pageTitle: 'Contacts',
+          pageContent: 'This is the contacts page'
+        }
+      ]
     }
   }
 }
